89a218733259f25b76f69cdbbd5552008baf3ee2
Filip Napiorkowski <filip.napiorkowski@xtremepush.com>
2022-05-26 14:44:24 +0100

    Finalised the BMI program with additional spec for determining the classification based on calculated BMI

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-height = input("Enter your height in m: ")
-weight = input("Enter your weight in kg: ")
+# Body Mass Index Calculator
+# Created by Filip Napiorkowski 2022
 
+# Get the user's input for height and weight
+height = input("Enter your height in m: \n")
+weight = input("Enter your weight in kg: \n")
+
+# Convert the input into float numbers
+float_height = float(height)
+float_weight = float(weight)
+
+# Calculate the body mass index using the formula
+body_mass_index = (float_weight / (float_height / 100) ** 2)
+
+#  Round up the result to the nearest 2 decimal places
+print(round(body_mass_index, 2))
+
+# Print out the corresponding result based on the calculated number
+if body_mass_index < 18.5:
+    print("You are underweight")
+elif body_mass_index > 18.5 and body_mass_index < 24.9:
+    print("You are normal weight")
+elif body_mass_index > 25 and body_mass_index < 29.9:
+    print("You are overweight")
+else:
+    print("You are obese")
